a0b98056c718e1ec4dc4d5e2726d40c960bd38e9
Jamal .S <me@shrjamal.com>
2020-11-23 17:57:51 +0100

    add CountryPicker component

---
+++ src/components/CountryPicker.tsx
@@ -0,0 +1,53 @@
+import {
+  Select,
+  makeStyles,
+  Theme,
+  createStyles,
+  FormControl,
+  MenuItem,
+} from "@material-ui/core";
+import React from "react";
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    formControl: {
+      margin: theme.spacing(1),
+      minWidth: 200,
+    },
+    selectEmpty: {
+      marginTop: theme.spacing(2),
+    },
+  })
+);
+
+const CountryPicker = ({
+  selected,
+  countries,
+  onChange,
+}: {
+  selected: string;
+  countries: string[];
+  onChange: (value: string) => void;
+}) => {
+  const classes = useStyles();
+  return (
+    <FormControl className={classes.formControl}>
+      <Select
+        value={selected}
+        variant="outlined"
+        onChange={(e) => onChange(e.target.value as string)}
+      >
+        <MenuItem key={0} value={"global"}>
+          Global
+        </MenuItem>
+        {countries.map((c) => (
+          <MenuItem key={c} value={c}>
+            {c}
+          </MenuItem>
+        ))}
+      </Select>
+    </FormControl>
+  );
+};
+
+export default CountryPicker;
