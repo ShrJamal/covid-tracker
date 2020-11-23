976d752ccc915fcdb3f31cecec5d89920024509b
Jamal .S <me@shrjamal.com>
2020-11-23 17:58:34 +0100

    add helpers

---
+++ src/helpers/map_circles.ts
@@ -0,0 +1,20 @@
+export const casesTypeColors = {
+  cases: {
+    hex: "#CC1034",
+    muliplier: 400,
+  },
+  recovered: {
+    hex: "#7dd71d",
+    muliplier: 1000,
+  },
+  deaths: {
+    hex: "#fb4443",
+    muliplier: 1600,
+  },
+};
+
+export enum CasesType {
+  cases = "cases",
+  recovered = "recovered",
+  deaths = "deaths",
+}
