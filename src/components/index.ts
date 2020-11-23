42f4f950e8ae3ab8d911968d125aa66336b2d05a
Jamal .S <me@shrjamal.com>
2020-11-23 17:59:45 +0100

    refactor entire app

---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+export { default as Cards } from "./Cards";
+export { default as Chart } from "./Chart";
+export { default as CountryPicker } from "./CountryPicker";
+export { default as CasesMap } from "./Map";
